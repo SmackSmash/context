ded2d1847a8d71f1fefd2ae9d06e39617653e24a
Dan Moffat <danmoffat@gmail.com>
2025-07-13 16:02:31 +0100

    Added a nice hover effect to show controls

--- src/components/ShowTodo.tsx
+++ src/components/ShowTodo.tsx
@@ -1,16 +1,26 @@
-import type { FC } from 'react';
+import { useState, type FC } from 'react';
 import type { Todo } from '../types';
 
 const ShowTodo: FC<{ todo: Todo }> = ({ todo }) => {
+  const [showControls, setshowControls] = useState(false);
+
   return (
-    <div className='flex items-center rounded-lg bg-indigo-300 py-2 pr-2 pl-4 text-indigo-950'>
-      <p>{todo.todo}</p>
-      <button className='ml-auto cursor-pointer rounded-lg bg-indigo-800 px-4 py-2 text-indigo-50'>
-        Edit
-      </button>
-      <button className='ml-2 cursor-pointer rounded-lg bg-red-800 px-4 py-2 text-indigo-50'>
-        Delete
-      </button>
+    <div
+      onMouseOver={() => setshowControls(true)}
+      onMouseLeave={() => setshowControls(false)}
+      className='flex items-center rounded-lg bg-indigo-300 py-2 pr-2 pl-4 text-indigo-950'
+    >
+      <p className='py-2'>{todo.todo}</p>
+      {showControls ? (
+        <>
+          <button className='ml-auto cursor-pointer rounded-lg bg-indigo-800 px-4 py-2 text-indigo-50'>
+            Edit
+          </button>
+          <button className='ml-2 cursor-pointer rounded-lg bg-red-800 px-4 py-2 text-indigo-50'>
+            Delete
+          </button>{' '}
+        </>
+      ) : null}
     </div>
   );
 };
