b1a0e8e1429325a56094b54cc3c3fc3560511f5e
Dan Moffat <danmoffat@gmail.com>
2025-07-13 15:56:28 +0100

    Fleshed out todo items

--- src/components/ShowTodo.tsx
+++ src/components/ShowTodo.tsx
@@ -1,7 +1,18 @@
 import type { FC } from 'react';
+import type { Todo } from '../types';
 
-const ShowTodo: FC = () => {
-  return <div>Show Todo</div>;
+const ShowTodo: FC<{ todo: Todo }> = ({ todo }) => {
+  return (
+    <div className='flex items-center rounded-lg bg-indigo-300 py-2 pr-2 pl-4 text-indigo-950'>
+      <p>{todo.todo}</p>
+      <button className='ml-auto cursor-pointer rounded-lg bg-indigo-800 px-4 py-2 text-indigo-50'>
+        Edit
+      </button>
+      <button className='ml-2 cursor-pointer rounded-lg bg-red-800 px-4 py-2 text-indigo-50'>
+        Delete
+      </button>
+    </div>
+  );
 };
 
 export default ShowTodo;
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,11 +1,12 @@
 import type { FC } from 'react';
 import type { Todo } from '../types';
+import ShowTodo from './ShowTodo';
 
 const TodoList: FC<{ todos: Todo[] }> = ({ todos }) => {
   return (
-    <div>
+    <div className='flex flex-col gap-2 shadow-xl'>
       {todos.map(({ id, todo }) => (
-        <div key={id}>{todo}</div>
+        <ShowTodo key={id} todo={{ id, todo }} />
       ))}
     </div>
   );
